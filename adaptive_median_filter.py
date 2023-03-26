da8be6035f914bbc9b816495d8085716c3fd329a
B0rjomi <valerkalifanov@gmail.com>
2023-03-26 17:10:19 +0300

    финальные изменения в коде адаптивного медианного фильтра

--- adaptive_median_filter.py
+++ adaptive_median_filter.py
@@ -2,38 +2,6 @@ from PIL import Image
 import numpy as np
 
 
-# def adaptive_median_filter_color(img, window_size_max):
-#     img_arr = np.array(img)
-#     h, w, c = img_arr.shape
-#     window_size = 3
-#
-#     out_arr = np.zeros_like(img_arr)
-#
-#     for j in range(h):
-#         for i in range(w):
-#             while window_size <= window_size_max:
-#                 # Get current window
-#                 window = img_arr[max(0, j - window_size//2):min(h, j + window_size//2 + 1),
-#                 max(0, i - window_size//2):min(w, i + window_size//2 + 1), :]
-#                 # Calculate median and max/min pixel value in each channel of the window
-#                 med = np.median(window, axis=(0,1))
-#                 max_val = np.max(window, axis=(0,1))
-#                 min_val = np.min(window, axis=(0,1))
-#
-#                 # Check if pixel value is within the allowed range in each channel
-#                 if np.all(min_val < med) and np.all(med < max_val):
-#                     out_arr[j, i, :] = med
-#                     break
-#                 else:
-#                     window_size += 2
-#             else:
-#                 out_arr[j, i, :] = med
-#
-#             window_size = 3
-#
-#     return Image.fromarray(out_arr)
-
-
 def level_A(z_min, z_med, z_max, z_xy, S_xy, S_max):
     if (z_min < z_med < z_max):
         return level_B(z_min, z_med, z_max, z_xy, S_xy, S_max)
