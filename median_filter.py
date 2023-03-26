97f7f04f5f446da5a4ed309c6c6b494365862b87
B0rjomi <valerkalifanov@gmail.com>
2023-03-26 16:28:20 +0300

    удалено большее количество изображений

--- median_filter.py
+++ median_filter.py
@@ -8,9 +8,7 @@ def median_filter(image, size):
     final_pic = []
     final_pic = np.zeros((len(image), len(image[0])))
     for i in range(len(image)):
-
         for j in range(len(image[0])):
-
             for z in range(size):
                 if i + z - pad < 0 or i + z - pad > len(image) - 1:
                     for c in range(size):
@@ -21,21 +19,22 @@ def median_filter(image, size):
                     else:
                         for k in range(size):
                             temp.append(image[i + z - pad][j + k - pad])
-
             temp.sort()
             final_pic[i][j] = temp[len(temp) // 2]
             temp = []
     return final_pic
 
+
 def show_res(path):
     global img
     try:
-        img = Image.open(path)
+        img = Image.open(path).convert('L')
     except FileNotFoundError:
         print("файла по заданному пути не существует")
     arr = np.array(img)
-    filtered_img = median_filter(arr, 5)
+    filtered_img = median_filter(arr, 7)
     img = Image.fromarray(filtered_img)
     img.show()
+
 
 
